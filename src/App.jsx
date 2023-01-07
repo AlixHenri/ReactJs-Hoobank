e61121fe0fea617bb8b1ebd634fa09b783e63cfd
AlyxHenri <matheushenrif@gmail.com>
2023-01-07 19:55:08 -0300

    Project Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import styles from './style'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div className="bg-primary w-full overflow-hidden">
+      <h1>Hello, World</h1>
+    </div>
+    )
 }
 
 export default App
